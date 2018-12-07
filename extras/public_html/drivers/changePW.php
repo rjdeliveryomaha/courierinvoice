4efc3e97e3bfe79e022d60b59815f16a62709fbd
Paul Pierce <pierce.paul.david@gmail.com>
2018-12-07 10:45:19 -0600

    Added password management to drivers

---
+++ extras/public_html/drivers/changePW.php
@@ -0,0 +1,17 @@
+<?php
+  if ($_SERVER['REQUEST_METHOD'] !== 'POST') return FALSE;
+  // Include functions
+  require_once "../../includes/sip_secSession.php";
+  
+  if (!is_sec_session_started()) sec_session_start();
+  
+  require_once '../../includes/APIToolsConfig.php';
+  require_once '../../vendor/autoload.php';
+  use rjdeliveryomaha\courierinvoice\Client;
+  
+  $client = new Client($config, $_POST);
+  if ($client === false) {
+    return $client->getError();
+  }
+  return $client->changePassword();
+  
