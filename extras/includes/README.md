106ec4b55a3c5a23254d6016a68f0af214eb9b63
Paul D. Pierce <pierce.paul.david@gmail.com>
2018-12-10 13:23:05 -0600

    Moved documentation from config file to README.

--- extras/includes/README.md
+++ extras/includes/README.md
@@ -103,7 +103,7 @@ Maximum number of months to display on a chart. Default is 6.
 
 String
 
-Login name for Courier Invoice user as an alternative to using ClientID 0 (zero).
+Login name for Courier Invoice user as an alternative to using client ID 0 (zero).
 
 ### driverChargesEntryExclude
 
@@ -158,5 +158,115 @@ This setting removes charges for client ticket query form.
 Index 0 admin clients.
 
 Index 1 daily clients.
+
+### orgChargesQueryExclude
+
+Indexed array
+
+This setting removes charges for organizations ticket query form.
+
+### client0ChargesEntryExclude
+
+Indexed array
+
+This setting removes charges for Courier Invoice user client 0 ticket entry and update form.
+
+### client0ChargesQueryExclude
+
+Indexed array
+
+This setting removes charges for Courier Invoice user client 0 ticket query form.
+
+### initialCharge
+
+Integer
+
+By default the Charge property is null when the ticket entry form is initialized.
+
+This setting selects a Charge value the ticket entry form is initialized.
+
+### extend
+
+Associative array
+
+Extend functionality with custom menu items, pages, and javascript.
+
+The top level keys are who to create the items for; all, client, org, driver, dispatcher, client0.
+
+Entries are indexed arrays with the following content:
+
+```php
+
+[ 'Menu Item', 'function_name', '../path/to/javascript.js', 'jsAttribute', 'jsAttribute' ]
+
+```
+
+Index 0 will be added, as is, to the menu. It will then have any HTML tags striped, be converted to lowercase, spaces replaced with underscore and used as the id attribute of the page.
+
+Index 1, if set and not null or an empty string, will be looked for first as a method in the Ticket, Route, Invoice, and Client classes then as a function in includes/user_functions.php to populate the page.
+
+Index 2, if set and not null or an empty string, will be added as the src of a script element.
+
+If an entry has a non-null, not empty string at index 0 and a null or empty string at index 1 it will be moved to the end of the list. This is done to preserve the indexing of entries to pages.
+
+If both index 0 and 1 are null or empty string index 2 will be added as the src of a script element. All scripts are added to the page in the order they are encountered in this configuration.
+
+Any indices beyond 2 will be interpreted as attributes to be applied to the script for example defer or async.
+
+  - __all__
+
+    Indexed array of settings entries as described above.
+
+    These items will be added for all users.
+
+  - __client__
+
+    Indexed array with three indices.
+
+    Each index is an indexed array of settings entries as described above.
+
+    * 0: All clients.
+
+    * 1: Admin clients.
+
+    * 2: Daily user clients.
+
+    These entries are not applied to organization users.
+
+    These entries are not applied to Courier Invoice user client ID 0 (zero).
+
+  - __org__
+
+    Indexed array of settings entries as described above.
+
+    These items will be added for only organization users.
+
+  - __driver__
+
+    Indexed array with three indices.
+
+    Each index is an indexed array of settings entries as described above.
+
+    * 0: All drivers.
+
+    * 1: Drivers that cannot dispatch.
+
+    * 2: Drivers that can dispatch only to themselves.
+
+    * 3: Drivers that can dispatch to themselves and other.
+
+  - __dispatch__
+
+    Indexed array of settings entries as described above.
+
+  - __client0__
+
+    Indexed array of settings entries as described above.
+
 ---
+
+Providing your basic Courier Invoice configuration options is necessary when using the CommonFunctions class without a session. An example is provided at the end of [api_config.php](https://github.com/rjdeliveryomaha/courierinvoice/blob/master/extras/includes/api_config.php).
+
+---
+
 # user_functions
--- extras/includes/api_config.php
+++ extras/includes/api_config.php
@@ -1,80 +1,53 @@
 <?php
   $config = [
     'username' => '',
+
     'publicKey' => '',
+
     'privateKey' => '',
-    // bool: logging is disabled by default
+
     'enableLogging' => false,
-    // string: if logging is enabled a target file must be defined
+
     'targetFile' => './API.log',
-    // Canceled tickets are not displayed on invoices by default
-    // indexed array of integers: client IDs to exempt from this rule
+
     'showCancelledTicketsOnInvoiceExceptions' => [],
-    // Contract tickets are consolidated by run number on invoices by default
-    // indexed array of integers: client IDs to exempt from this rule
+
     'consolidateContractTicketsOnInvoiceExceptions' => [],
-    // associative array: client names that should be changed, for example, to abbreviate
-    // ex: [ 'some long client name' => 'SLCN']
+
     'clientNameExceptions' => [],
-    // indexed array: addresses that should be ignored, for example, due to change of address
+
     'clientAddressExceptions' => [],
-    // indexed array: Values that should not be included on ticket entry datalists. Values should be lower case.
+
     'ignoreValues' => [ 'none', 'test', 'billing correction', '-', 'multiple', 'fuel compensation', 'other charge' ],
-    // associative array: Setting to use with PHPMailer.
+
     'emailConfig' => [ 'fromAddress' => '', 'password'=> '', 'smtpHost' => '', 'port' => '587', 'secureType' => 'tls', 'fromName' => '', 'BCCAddress' => '' ],
-    // integer: maximum number of months to display on a chart. Default is 6
+
     'allTimeChartLimit' => 6,
-    // string: login name for Courier Invoice user as an alternative to using ClientID 0 (zero)
+
     'userLogin' => 'CustomLogin',
-    // By default all charges are included on ticket forms
-    // The following settings remove charges based on the user type and form type
-    // 2 dimentional indexed array of charges to exclude for drivers ticket enrty and update form
-    // index 0 driver can dispatch to self
-    // index 1 driver can dispatch to all
+
     'driverChargesEntryExclude' => [ [ 0, 8, 9 ], [ 0, 8, 9 ] ],
-    // 2 dimentional indexed array of charges to exclude for drivers ticket query form
-    // index 0 driver can dispatch to self
-    // index 1 driver can dispatch to all
+
     'driverChargesQueryExclude' => [ [ 0, 8, 9 ], [ 0, 8, 9 ] ],
-    // indexed array of charges to exclude for dispatchers ticket entry and update forms
+
     'dispatchChargesEntryExclude' => [ 0, 8, 9 ],
-    // indexed array of charges to exclude for dispatchers ticket query forms
+
     'dispatchChargesQueryExclude' => [ 9 ],
-    // 2 dimentional indexed array of charges to exclude for clients entry (request) form
-    // index 0 = admin clients
-    // index 1 = daily clients
+
     'clientChargesEntryExclude' => [ [ 0, 8, 9 ], [ 0, 8, 9 ] ],
-    // 2 dimentional indexed array of charges to exclude for clients query form
-    // index 0 = admin clients
-    // index 1 = daily clients
+
     'clientChargesQueryExclude' => [ [], [ 0, 8, 9 ] ],
-    // indexed array of charges to exclude for organizations query from
+
     'orgChargesQueryExclude' => [],
-    // indexed array of charges to exclude for Courier Invoice user client 0 entry and update forms
+
     'client0ChargesEntryExclude' => [],
-    // indexed array of charges to exclude for Courier Invoice user client 0 query form
+
     'client0ChargesQueryExclude' => [],
-    // By default Charge is null when the ticket entry form is initialized
-    // number: Charge to be selected when ticket entry form is initialized
+
     'initialCharge' => 5,
-    // associative array extend layout and menue with custom pages
-    // The top level keys are who to create the custom pages for; all, client, org, driver, dispatcher, client0.
-    // all, org, and dispatcher should contain a single indexed array
-    // Index 0 will be added, as is, to the menue. It will be converted to lowercase, spaces replaced with underscore and used as the id attribute of the page.
-    // Index 1, if set and not null or an empty string, will be looked for first as a method in the Ticket, Route, Invoice, and Client classes then as a function in includes/user_functions.php to populate the page.
-    // This function should return html content.
-    // Index 2, if set and not null or an empty string, will be added as the src of a script element.
-    // If an entry has a non-null, not empty string at index 0 and a null or empty string at index 1 it will be moved to the end of the list.
-    // This is done to preserve the indexing of entries to pages.
-    // If both index 0 and index 1 are null or empty string index 2 will be added as the src of a script element.
-    // Any indices past 2 will be interpreted as properties to be applied to the script ex: defer or async.
-    // The client and driver keys should a multi-dimentional indexed array with elements as described above
-    // 'client' => [ [ ['all clients'] ], [ ['admin clients'] ], [ ['daily clients'] ] ]
-    // 'drivers' => [ [ ['all drivers'] ], [ ['driver cannot dispatch'] ], [ ['driver can dispatch self'] ], [ ['driver can dispatch any'] ] ]
+
     'extend' => [
       'all' => [
-//        ['Help', 'createHelpContent', '../js/help.js'], // This item would be moved to the end of the menue
-//        [ null, null, '../js/maps.js'], // This item would be added as the first script on the page
         [null, null, '../app_js/jQuery.ajaxRetry.min.js'],
         [null, null, '../app_js/ajaxTemplate.js'],
         [null, null, '../app_js/app.js']
@@ -82,7 +55,6 @@
       'client' => [
         [
           // All clients
-//          [null, null, 'https://remote.script.com/do_something', 'async', 'defer'],
           ['Ticket Entry', 'ticketForm'],
           ['Ticket Query', 'ticketQueryForm']
         ],
