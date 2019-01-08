adc9763c369cdeef8a86d35c330c03434e928fe7
Paul Pierce <pierce.paul.david@gmail.com>
2019-01-08 17:34:23 -0600

    Better database error catching.

--- extras/public_html/login.php
+++ extras/public_html/login.php
@@ -1,42 +1,44 @@
 <?php
-  include_once '../includes/api_config.php';
+  include_once '../includes/APIToolsConfig.php';
   include_once '../vendor/autoload.php';
-  include_once '../includes/user_functions.php';
-
+  include_once '../includes/sip_secSession.php';
+  
   use rjdeliveryomaha\courierinvoice\LoginHandler;
   use rjdeliveryomaha\courierinvoice\SecureSessionHandler;
-
-  if ($_SERVER['REQUEST_METHOD'] !== "POST") return false;
+  
+  if ($_SERVER['REQUEST_METHOD'] !== 'POST') return false;
   if (isset($_POST['brute'])) {
     $uname = $_POST['clientID'];
     // Create connection
     $conn = getLoginConnection();
     if (!is_object($conn)) {
-      return true;
+      echo "<span data-value=\"break\" class=\"error\">{$conn}</span>";
+      return false;
     }
-    $sql = "INSERT INTO attempt (user) VALUES (?)";
+    $sql = 'INSERT INTO attempt (user) VALUES (?)';
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('s', $uname);
     if (!$stmt->execute()) {
-      echo '<span data-value="break" class="error">Server Error ' . $stmt->errno . 'Line ' . __line__ . ':</span>' . $stmt->error;
-      return FALSE;
+      echo "<span data-value=\"break\" class=\"error\">Server Error {$stmt->errno}</span>: {$stmt->error}";
+      return false;
     } else {
-      return FALSE;
+      return false;
     }
   }
-
+  
   try {
     SecureSessionHandler::start_session($config);
   } catch(Exception $e) {
     echo $e->getMessage();
-    return FALSE;
+    return false;
   }
-
+  
   if (isset($_SESSION['brute']) && in_array($_POST['clientID'], json_decode($_SESSION['brute'], TRUE), TRUE)) {
-    echo '<span data-value="break" class="error">Account locked.<br>Too many failed login attempts.</span><span class="hide">Line ' . __line__ . '</span>';
-    return FALSE;
+    echo '<span data-value="break" class="error">Account locked.<br>Too many failed login attempts.</span>';
+    return false;
   }
-  if (checkbrute($_POST) === TRUE) {
+  $bruteCheck = checkbrute($_POST);
+  if ($bruteCheck === TRUE) {
     if (isset($_SESSION['brute'])) {
       $bruteList = json_decode($_SESSION['brute'], TRUE);
       if (!in_array($uname, $bruteList, TRUE)) {
@@ -48,20 +50,23 @@
     else {
       $_SESSION['brute'] = json_encode(array($uname));
     }
-    echo '<span data-value="break" class="error">Account locked.<br>Too many failed login attempts.</span><span class="hide">Line ' . __line__ . '</span>';
-    return FALSE;
+    echo '<span data-value="break" class="error">Account locked.<br>Too many failed login attempts.</span>';
+    return false;
+  } elseif (!is_bool($bruteCheck)) {
+    echo $bruteCheck;
+    return false;
   }
   try {
     $handler = new LoginHandler($config, $_POST);
   } catch(Exception $e) {
     echo $e->getMessage();
-    return FALSE;
+    return false;
   }
   try {
     $url = $handler->login();
   } catch(Exception $e) {
     echo $e->getMessage();
-    return FALSE;
+    return false;
   }
   echo $url;
-  return FALSE;
+  return false;
