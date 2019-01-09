d0428493c104af9d9cc8a8c3f7825d0a57267159
Paul Pierce <pierce.paul.david@gmail.com>
2019-01-08 18:06:17 -0600

    fixed typo

--- extras/public_html/index.php
+++ extras/public_html/index.php
@@ -22,8 +22,8 @@
 <html>
 <head>
   <meta charset="UTF-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width">
   <meta name="mobile-web-app-capable" content="yes">
   <meta name="apple-mobile-web-app-capable" content="yes">
 	
