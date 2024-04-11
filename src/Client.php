ff4400ffd4d0c1d6fffeed748e7992b37032c80e
resident0 <pierce.paul.david@gmail.com>
2024-04-11 12:57:52 -0500

    Better solution for client instantiation

--- src/Client.php
+++ src/Client.php
@@ -58,7 +58,7 @@
       } catch (\Exception $e) {
         throw $e;
       }
-      foreach ($this->config as $key => $value) {
+      foreach ($_SESSION['members'][$this->ClientID] as $key => $value) {
         if (property_exists($this, $key)) {
          $this->$key = $value;
         }
